5c3924c96e89be81e488cd777fb0cb8b1c71e4bf
Rahul-Verma <94426476+starcaptian@users.noreply.github.com>
2022-02-05 00:18:04 +0530

    component added by starcaptian

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Rv from './components/Rv/Rv';
 function App() {
   return (
     <div>
     <h1> MY APP</h1>
+    <Rv/>
     <p></p>
+
     </div>
   );
 }
---
+++ src/components/Rv/Rv.js
@@ -0,0 +1,6 @@
+import React from 'react';
+const Rv = () => {
+    return <h1>this is added by user2</h1>
+
+}
+export default Rv;
