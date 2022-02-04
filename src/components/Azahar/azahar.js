69339137289d95f47ca8d6d7e92471d2780ca06c
azahar200 <96261861+azahar200@users.noreply.github.com>
2022-02-05 00:39:41 +0530

    azahar

---
+++ src/components/Azahar/azahar.js
@@ -0,0 +1,6 @@
+import React from 'react';
+const Rv = () => {
+    return <h1>this is added by  Azahar</h1>
+
+}
+export default Rv;
--- src/components/Mycomponent/Azahar/azahar.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-const Rv = () => {
-    return <h1>this is added by Azahar</h1>
-
-}
-export default Rv;
